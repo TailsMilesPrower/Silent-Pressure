a9c6a465dd14bc377643d5c7c6af189f26d0653d
Lyuboslav Nalbantov <tails969@gmail.com>
2025-10-17 11:33:02 +0200

    Change Scene script

---
+++ Assets/_Scripts/SceneChangeCollider.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeCollider : MonoBehaviour
+{
+
+    [SerializeField] private string SceneName;
+    [SerializeField] private string targetTag = "Player";
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(targetTag))
+        {
+            SceneManager.LoadScene("ComingSoon");
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
