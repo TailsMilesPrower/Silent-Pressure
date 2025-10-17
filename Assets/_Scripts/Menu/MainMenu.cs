4a93c94bedf2dbfb8674db653388d8dffd03f7c8
Lyuboslav Nalbantov <tails969@gmail.com>
2025-10-17 10:40:22 +0200

    Main Menu and Credits

---
+++ Assets/_Scripts/Menu/MainMenu.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void startGame()
+    {
+        SceneManager.LoadScene("Level01_TrainLevel");
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void backToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
