0b37325216b6074ac09cb61023518612b1ad8631
Lyuboslav Nalbantov <tails969@gmail.com>
2025-10-22 20:00:53 +0200

    Small changes to SceneChangeCollider

--- Assets/_Scripts/SceneChangeCollider.cs
+++ Assets/_Scripts/SceneChangeCollider.cs
@@ -3,16 +3,41 @@ using UnityEngine.SceneManagement;
 
 public class SceneChangeCollider : MonoBehaviour
 {
+    [Header("Scene Settings")]
+    [Tooltip("Name of the scene to load. Leave empty to use build index.")]
+    [SerializeField] private string sceneName;
 
-    [SerializeField] private string SceneName;
+    //public int sceneBuildIndex = -1;
+
+    [Header("Trigger Settings")]
     [SerializeField] private string targetTag = "Player";
-    
+
+    private void Reset()
+    {
+        GetComponent<Collider>().isTrigger = true;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag(targetTag))
+        if (!other.CompareTag(targetTag)) return;
+
+
+        if (!string.IsNullOrEmpty(sceneName))
         {
-            SceneManager.LoadScene("Upgrades");
+            SceneManager.LoadScene(sceneName);
         }
+        /*
+        else if (sceneBuildIndex >= 0)
+        {
+            SceneManager.LoadScene(sceneBuildIndex);
+        }
+        */
+        else
+        {
+            Debug.LogWarning($"{name}: No valid scene assigned to load!");
+        }
+
+
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
