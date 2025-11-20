5629cdfb127b163cfac0a6611cc336674bf5e053
Rayas <irasiserar@gmail.com>
2025-11-20 15:09:20 +0100

    Library Entrence model + Finished car movement

---
+++ Assets/SID/CarMove1.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CarMove1: MonoBehaviour
+{
+    public float speed = 2f;
+    private float distanceToTravel = 634f;
+    private float distanceMoved = 0f;
+
+    private void Update()
+    {
+        float moveStep = speed * Time.deltaTime;
+
+        transform.Translate(0f, 0f, -moveStep);
+
+        distanceMoved += moveStep;
+
+        if (distanceMoved >= distanceToTravel)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
